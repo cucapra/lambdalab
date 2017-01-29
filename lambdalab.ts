945cc816ee1af1e02e6554e05f1f18f4db0ac287
Adrian Sampson <adrian@radbox.org>
2017-01-29 16:58:13 -0500

    Hijack input to type lambdas

--- lambdalab.ts
+++ lambdalab.ts
@@ -1,1 +1,33 @@
-console.log("hi");
+// Insert text into the DOM at the current caret.
+function insertText(text: string) {
+  let sel = window.getSelection();
+  if (sel.getRangeAt && sel.rangeCount) {
+    // Remove any current contents.
+    let range = sel.getRangeAt(0);
+    range.deleteContents();
+
+    // Add the new text to the DOM.
+    let node = document.createTextNode(text);
+    range.insertNode(node);
+
+    // Put the selection (caret) after the newly-inserted text.
+    let newRange = document.createRange();
+    newRange.setStartAfter(node);
+    sel.removeAllRanges();
+    sel.addRange(newRange);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let programBox = document.getElementById("program");
+  programBox!.addEventListener("keypress", (event) => {
+    // When the user types \, insert a lambda instead.
+    if (event.key === "\\") {
+      // Don't insert the \ character.
+      event.preventDefault();
+
+      // Instead, we'll insert a lambda.
+      insertText("λ");
+    }
+  });
+});
