f532546e449336b6169e97da40c0ed6aca338d56
Adrian Sampson <adrian@radbox.org>
2017-02-11 20:14:34 -0500

    Run a basic test

--- test/test_parse.ts
+++ test/test_parse.ts
@@ -1,1 +1,9 @@
-import * as tape from 'tape';
+import * as test from 'tape';
+import { parse } from '../lib/parse';
+import { pretty } from '../lib/ast';
+
+test('variable', (t) => {
+  let res = pretty(parse("x")!);
+  t.equal(res, "x");
+  t.end();
+});
